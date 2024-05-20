25cee978223f39acf7caa41b466064206603dc01
balldev1 <balldev1@hotmail.com>
2024-05-20 16:19:38 +0700

    first commit

--- obj/Debug/net8.0/RestSample.AssemblyInfo.cs
+++ obj/Debug/net8.0/RestSample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5247809009170093b5b1e4d21dba3e85a1f0f90f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a84ac238d179317b711ad4e089c64610822d73d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
